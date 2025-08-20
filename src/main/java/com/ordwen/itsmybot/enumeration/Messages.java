adebac37b257a09edba1f957ee033f68e566b92f
Ordwen <samuelespallier77@gmail.com>
2025-08-20 18:51:16 +0200

    misc: updating default settings & messages

--- src/main/java/com/ordwen/itsmybot/enumeration/Messages.java
+++ src/main/java/com/ordwen/itsmybot/enumeration/Messages.java
@@ -1,6 +1,5 @@
 package com.ordwen.itsmybot.enumeration;
 
-import com.ordwen.itsmybot.configuration.essential.Prefix;
 import com.ordwen.itsmybot.file.implementation.MessagesFile;
 import com.ordwen.itsmybot.util.TextFormatter;
 import org.bukkit.Bukkit;
@@ -9,30 +8,31 @@ import org.bukkit.entity.Player;
 
 public enum Messages {
 
-    PLAYER_ONLY("player_only", "<red>This command can only be executed by a player!</red>"),
-    NO_PERMISSION("no_permission", "<red>You do not have permission to execute this command!</red>"),
+    PLAYER_ONLY("player_only", "<prefix> <#E0E3FF>This command can only be executed by a player!</#E0E3FF>"),
+    NO_PERMISSION("no_permission", "<prefix> <#E0E3FF>You do not have permission to execute this command!</#E0E3FF>"),
     PLAYER_HELP("player_help", String.join("\n",
-            "<green>Player commands:</green>",
-            "<yellow>/discord <green>: show the Discord link</green>",
-            "<yellow>/link <green>: link your Discord account</green>",
-            "<yellow>/unlink <green>: unlink your Discord account</green>",
-            "<yellow>/discord claim <green>: claim your Discord rewards</green>"
+            "<#5865F2>ᴘʟᴀʏᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ:</#5865F2>",
+            "<#5865F2>/discord <#E0E3FF>: show the Discord link</#E0E3FF>",
+            "<#5865F2>/link <#E0E3FF>: link your Discord account</#E0E3FF>",
+            "<#5865F2>/unlink <#E0E3FF>: unlink your Discord account</#E0E3FF>",
+            "<#5865F2>/discord claim <#E0E3FF>: claim your Discord rewards</#E0E3FF>"
     )),
     ADMIN_HELP("admin_help", String.join("\n",
-            "<green>Admin commands:</green>",
-            "<yellow>/discord reload <green>: reload the plugin configuration</green>"
+            "<#5865F2>ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅꜱ:</#5865F2>",
+            "<#5865F2>/discord reload <#E0E3FF>: reload the plugin configuration</#E0E3FF>"
     )),
-    ERROR_OCCURRED("error_occurred", "<red>An error occurred while processing your request. Please contact support.</red>"),
-    LINK_SUCCESS("link_success", "<green>Your Discord account has been successfully linked!</green>"),
-    UNLINK_SUCCESS("unlink_success", "<green>Your Discord account has been successfully unlinked!</green>"),
-    ALREADY_LINKED("already_linked", "<red>Your Discord account is already linked!</red>"),
-    NOT_LINKED("not_linked", "<red>You have not linked your Discord account yet!</red> <yellow>Use</yellow> <yellow>/link</yellow> <red>to link it.</red>"),
-    CODE_REQUIRED("code_required", "<red>You must provide a code to link your Discord account!</red>"),
-    INVALID_CODE("invalid_code", "<red>The code you provided is invalid! Please try again.</red>"),
-    CLAIM_SUCCESS("claim_success", "<green>You have successfully claimed your Discord rewards!</green>"),
-    CLAIM_NO_REWARD("claim_no_reward", "<red>You have no rewards to claim at this time.</red>"),
-    PLUGIN_RELOADED("plugin_reloaded", "<yellow>Plugin reloaded. Please check the console for any errors.</yellow>"),
-    JOIN_DISCORD("join_discord", "<green>Join our Discord server to get advantages and link your account: <blue>https://discord.gg/yourserver</blue></green>")
+    ERROR_OCCURRED("error_occurred", "<prefix> <#E0E3FF>An error occurred while processing your request. Please contact support.</#E0E3FF>"),
+    BOT_NOT_CONNECTED("bot_not_connected", "<prefix> <#E0E3FF>The bot is not connected to Discord! Please inform an administrator.</#E0E3FF>"),
+    LINK_SUCCESS("link_success", "<prefix> <#E0E3FF>Your Discord account has been successfully linked!</#E0E3FF>"),
+    UNLINK_SUCCESS("unlink_success", "<prefix> <#E0E3FF>Your Discord account has been successfully unlinked!</#E0E3FF>"),
+    ALREADY_LINKED("already_linked", "<prefix> <#E0E3FF>Your Discord account is already linked!</#E0E3FF>"),
+    NOT_LINKED("not_linked", "<prefix> <#E0E3FF>You have not linked your Discord account yet! Use /link to link your account.</#E0E3FF>"),
+    CODE_REQUIRED("code_required", "<prefix> <#E0E3FF>To link your Discord account, join the <click:\"open_url\":\"https://discord.gg/yourserver\"><#5865F2>ᴅɪꜱᴄᴏʀᴅ</#5865F2></click>, run the /link command there, and follow the given steps.</#E0E3FF>"),
+    INVALID_CODE("invalid_code", "<prefix> <#E0E3FF>The code you provided is invalid! Please try again.</#E0E3FF>"),
+    CLAIM_SUCCESS("claim_success", "<prefix> <#E0E3FF>You have successfully claimed your rewards!</#E0E3FF>"),
+    CLAIM_NO_REWARD("claim_no_reward", "<prefix> <#E0E3FF>You have no rewards to claim at this time.</#E0E3FF>"),
+    PLUGIN_RELOADED("plugin_reloaded", " <prefix> <#E0E3FF>Plugin reloaded. Please check the console for any errors.</#E0E3FF>"),
+    JOIN_DISCORD("join_discord", "<prefix> <#E0E3FF>Join our Discord to stay updated and link your account!</#E0E3FF> <click:\"open_url\":\"https://discord.gg/yourserver\"><#5865F2>ᴄʟɪᴄᴋ ᴛᴏ ᴊᴏɪɴ →</#5865F2></click>")
 
     ;
 
@@ -50,9 +50,9 @@ public enum Messages {
         if (msg != null && !msg.trim().isEmpty()) {
             if (sender instanceof Player) {
                 final Player player = (Player) sender;
-                TextFormatter.send(player, Prefix.getPrefix() + msg);
+                TextFormatter.send(player, msg);
             } else {
-                Bukkit.getConsoleSender().sendMessage(TextFormatter.legacy(Prefix.getPrefix() + msg));
+                Bukkit.getConsoleSender().sendMessage(TextFormatter.legacy(msg.replace("<prefix>", "[ItsMyBot] ")));
             }
         }
     }
@@ -61,7 +61,7 @@ public enum Messages {
         String msg = MessagesFile.getInstance().get(this.path, defaultMessage);
 
         if (!msg.trim().isEmpty()) {
-            TextFormatter.send(player, Prefix.getPrefix() + msg);
+            TextFormatter.send(player, msg);
         }
     }
 
